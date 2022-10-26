6413d0053edeefc07b35562a04a5bca3f669fb86
Aditi Pandey <aditimech01@gmail.com>
2022-10-26 15:13:15 -0400

    add create parseAndDisplayName

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -1,1 +1,41 @@
-console.log('the script tag works');
+// console.log('the script tag works');
+
+// let fullName = "Aditi Pandey";
+// console.log(`The variable is fullName ${fullName}`);
+// console.log(typeof fullName);
+
+// let space = fullName.indexOf(' '); //position number starts from 0
+// console.log(space);
+
+// let firstName = fullName.substring(0, space);
+// console.log(firstName);
+
+// let lastName = fullName.substring(space + 1);
+// console.log(lastName);
+
+function parseAndDisplayName(name) {
+    let space = name.indexOf(' ');
+    let firstName = name.substring(0, space);
+    let lastName = name.substring(space + 1);
+    console.log(firstName, lastName, name );
+    
+}
+parseAndDisplayName('Kinshuk Tiwari');
+
+
+
+
+
+
+
+
+
+// function parseAndDisplayName() {
+//     let fullName = "Aditi Pandey";
+//     let space = fullName.indexOf(' ');
+//     let firstName = fullName.substring(0, space);
+//     let lastName = fullName.substring(space + 1);
+// }
+// parseAndDisplayName();
+
+
